65a379b6fc5b57376987d429b04a98bc93d0ebc8
Erik Antonio Alarcón Pinedo <C8VMM@ferrovial.com>
2026-09-05 14:42:45 -0500

    x

--- src/org/ocpsoft/rewrite/servlet/RewriteFilter.java
+++ src/org/ocpsoft/rewrite/servlet/RewriteFilter.java
@@ -371,6 +371,7 @@ public class RewriteFilter implements Filter {
 
                 String jwtRefreshToken = getCookieValue(request, "refreshToken");
 
+                System.out.println("======traceId="+traceId+" user = " + user+ " URI="+requestURI+" destinyRequest="+destinyRequest);
                 if (user != null
                         || jwtRefreshToken != null
                         || requestURI.startsWith("login")
@@ -381,7 +382,6 @@ public class RewriteFilter implements Filter {
                     {
                         X.DEBUG = true;
                         String destinyRequest = req.getParameter("destiny");
-                        System.out.println("======traceId="+traceId+" user = " + user+ " URI="+requestURI+" destinyRequest="+destinyRequest);
                         if (user != null && !contextPath.equals("")) {// verificar master session valida (mejorar usando
                                                                       // api/auth)
                             String mainSessionId = (String) session.getAttribute(MASTER_SESSION_ID);
